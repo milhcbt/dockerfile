89e254ad6a8e105493aa32ba2967f84e3f6a3b59
Dimas Restu H <dimas.restu@student.upi.edu>
2017-07-10 23:42:01 +0700

    Update Debian Base, Debian Apache, Debian Nginx

--- dimaskiddo-debian-8-apache-2.4/entry/entrypoint.sh
+++ dimaskiddo-debian-8-apache-2.4/entry/entrypoint.sh
@@ -3,38 +3,29 @@
 # Prepare Apache configuration file if the file doesn't exist
 # due to volume support
 if [ ! -f /var/www/config/apache2/apache2.conf ]; then
-  mkdir -p /var/www/config/apache2
-  cp /etc/apache2/apache2.conf.docker /var/www/config/apache2/apache2.conf
-fi
-if [ ! -f /var/www/config/apache2/ports.conf ]; then
-  mkdir -p /var/www/config/apache2
-  cp /etc/apache2/ports.conf.docker /var/www/config/apache2/ports.conf
+  sudo mkdir -p /var/www/config/apache2
+  sudo cp /etc/apache2/apache2.conf.docker /var/www/config/apache2/apache2.conf
 fi
 
 
 # Prepare PHP Apache configuration file if the file doesn't exist
 # due to volume support
 if [ ! -f /var/www/config/php5/apache2/php.ini ]; then
-  mkdir -p /var/www/config/php5/apache2
-  cp /etc/php5/apache2/php.ini.docker /var/www/config/php5/apache2/php.ini
+  sudo mkdir -p /var/www/config/php5/apache2
+  sudo cp /etc/php5/apache2/php.ini.docker /var/www/config/php5/apache2/php.ini
 fi
-
-
-# Setting-up Configuration directory permission if the file doesn't exist
-# due to volume support
-chmod -R 664 /var/www/config
 
 
 # Prepare Default Web Content file if the file doesn't exist
 # due to volume support
 if [ ! -f /var/www/html/index.php ]; then
-  cp /var/www/docker/index.php /var/www/html/index.php
-  cp /var/www/docker/info.php /var/www/html/info.php
+  sudo cp /var/www/docker/index.php /var/www/html/index.php
+  sudo cp /var/www/docker/info.php /var/www/html/info.php
 fi
 
 
 # Try to start Apache on non-daemonize mode
-/usr/sbin/apache2ctl -D FOREGROUND
+sudo /usr/sbin/apache2ctl -D FOREGROUND
 
 
 # End the script
--- dimaskiddo-debian-8-nginx-1.6.2/entry/entrypoint.sh
+++ dimaskiddo-debian-8-nginx-1.6.2/entry/entrypoint.sh
@@ -3,41 +3,41 @@
 # Prepare Nginx configuration file if the file doesn't exist
 # due to volume support
 if [ ! -f /var/www/config/nginx/nginx.conf ]; then
-  mkdir -p /var/www/config/nginx
-  cp /etc/nginx/nginx.conf.docker /var/www/config/nginx/nginx.conf
+  sudo mkdir -p /var/www/config/nginx
+  sudo cp /etc/nginx/nginx.conf.docker /var/www/config/nginx/nginx.conf
 fi
 if [ ! -f /var/www/config/nginx/sites-available/default ]; then
-  mkdir -p /var/www/config/nginx/sites-available
-  cp /etc/nginx/sites-available/default.docker /var/www/config/nginx/sites-available/default
+  sudo mkdir -p /var/www/config/nginx/sites-available
+  sudo cp /etc/nginx/sites-available/default.docker /var/www/config/nginx/sites-available/default
 fi
 
 
 # Prepare PHP-FPM configuration file if the file doesn't exist
 # due to volume support
 if [ ! -f /var/www/config/php5/fpm/php.ini ]; then
-  mkdir -p /var/www/config/php5/fpm
-  cp /etc/php5/fpm/php.ini.docker /var/www/config/php5/fpm/php.ini
+  sudo mkdir -p /var/www/config/php5/fpm
+  sudo cp /etc/php5/fpm/php.ini.docker /var/www/config/php5/fpm/php.ini
 fi
 if [ ! -f /var/www/config/php5/fpm/pool.d/www.conf ]; then
-  mkdir -p /var/www/config/php5/fpm/pool.d
-  cp /etc/php5/fpm/pool.d/www.conf.docker /var/www/config/php5/fpm/pool.d/www.conf
+  sudo mkdir -p /var/www/config/php5/fpm/pool.d
+  sudo cp /etc/php5/fpm/pool.d/www.conf.docker /var/www/config/php5/fpm/pool.d/www.conf
 fi
 
 
 # Prepare Default Web Content file if the file doesn't exist
 # due to volume support
 if [ ! -f /var/www/html/index.php ]; then
-  cp /var/www/docker/index.php /var/www/html/index.php
-  cp /var/www/docker/info.php /var/www/html/info.php
+  sudo cp /var/www/docker/index.php /var/www/html/index.php
+  sudo cp /var/www/docker/info.php /var/www/html/info.php
 fi
 
 
 # Try to start PHP-FPM on daemonize mode
-/usr/sbin/php5-fpm --daemonize
+sudo /usr/sbin/php5-fpm --daemonize
 
 
 # Try to start Nginx on non-daemonize mode
-/usr/sbin/nginx
+sudo /usr/sbin/nginx
 
 
 # End the script
